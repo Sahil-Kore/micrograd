77daeee48262220b1561e1643affc2c68ecbfe57
Sahil-Kore <sahilkore2345@gmail.com>
2025-06-19 04:42:56 +0530

    Created a beural network to predict the next letter given a bigram

--- makemore/makemore.py
+++ makemore/makemore.py
@@ -390,3 +390,248 @@ nll=-log_likelihood
 print("average negative log likelihood",nll/n)
 
 #now the nll is not infinity
+
+
+
+
+#training a neural network to predict bigrams
+
+#training sset of bigrams
+xs,ys=[],[]
+
+for w in words[:1]:
+    chs=['.'] + list(w) + ['.']
+    for ch1,ch2 in zip(chs,chs[1:]):
+        ix1=stoi[ch1]
+        ix2=stoi[ch2]
+        print(ch1,ch2)
+        xs.append(ix1)
+        ys.append(ix2)
+
+xs=torch.tensor(xs)
+ys=torch.tensor(ys)
+xs
+ys
+
+#torch.tensor infers the datatype automatically while torch.Tensor returns a float
+
+#we will have to one hot encode this data
+
+import torch.nn.functional as F
+
+xenc=F.one_hot(xs,num_classes=27)
+xenc.shape
+#five exmaples from a single word emma 
+plt.imshow(xenc)
+
+xenc.dtype
+#we are going input xenc to neural network so the datatype of the elements should be float
+xenc=F.one_hot(xs,num_classes=27).float()
+
+W=torch.randn((27,1))
+W
+xenc.shape
+#every row of xenc has 1 only for the current letter others are 0
+
+#this will multiply all example encodings with the weights to give the activations 
+xenc @  W
+
+#we can think of it as 27 inputs and a single output 
+#but we want the probabilites of all the 27 characters
+#27 inputs and 27 neurons
+
+
+#so every element says the firing rate for the column number neuron for the row number example 
+W=torch.randn((27,27))
+xenc @ W
+
+#the firing rate of 13th neuron looking at the third input
+xenc @ W[3:13]
+
+#columns of W contain the weights of the number of neuron 
+#3 rd column has weights for the 3rd neuron
+
+
+logits=xenc @ W #log-counts
+counts=logits.exp()
+prob= counts / counts.sum(1,keepdims=True)
+
+
+'''
+xenc @ W gave logits 
+we interpreted them to be log counts
+exponentiate them to get something like counts
+normalize them to get a probability distribution
+and all these operations are differentiable
+
+'''
+
+g=torch.Generator().manual_seed(2147483647)
+W=torch.randn((27,27),generator=g)
+
+xenc=F.one_hot(xs,num_classes=27).float()
+logits=xenc @ W
+counts= logits.exp()
+probs = counts/ counts.sum(1,keepdim=True)
+
+nlls=torch.zeros(5)
+for i in range(5):
+    x=xs[i].item()
+    y=ys[i].item()
+    
+    print("--------")
+    print(f"bigram example {i+1}: {itos[x]}{itos[y]} (indexes {x},{y})")
+    print("Input to the neural net :",x)
+    print("Output probabilities from the neural network",probs[i])
+    print("Label (actual next character):", y)
+    p=probs[i,y]
+    print("Probability assigned by the net to the correct character ",p.item())
+    logp=torch.log(p)
+    print('log likelihood',logp.item())
+    nll=-logp
+    print("Negative log likelihood ",nll.item())
+    nlls[i]=nll
+    
+print("average negative log likelihood i.e. loss =",nlls.mean().item())
+    
+    
+    
+g=torch.Generator().manual_seed(2147483647)
+W=torch.randn((27,27),generator=g,requires_grad=True)
+
+
+#forward pass
+xenc=F.one_hot(xs,num_classes=27).float()
+logits=xenc @ W
+counts= logits.exp()
+probs = counts/ counts.sum(1,keepdim=True)
+#the probabilites of the correct answers
+probs[torch.arange(5),ys]
+loss=-probs[torch.arange(5),ys].log().mean()
+loss
+
+#backward pass
+#setting gradient to none is more efficient
+W.grad=None
+loss.backward()
+W.grad
+
+#updating the tensors
+W.data+=-0.1*W.grad
+
+
+
+
+#training on the whole dataset
+
+xs,ys=[],[]
+for w in words:
+    chs =['.'] + list(w) + ["."]
+    for ch1,ch2 in zip(chs,chs[1:]):
+        ix1=stoi[ch1]
+        ix2=stoi[ch2]
+        xs.append(ix1)
+        ys.append(ix2)
+xs=torch.tensor(xs)
+ys=torch.tensor(ys)
+#length of training data
+
+num=xs.nelement()
+print("Number of examples ",num)
+
+#initialize the network
+g=torch.Generator().manual_seed(2147483647)
+W=torch.randn((27,27),generator=g,requires_grad=True)
+xenc=F.one_hot(xs,num_classes=27).float()
+
+#gradient descent
+
+for k in range(100):
+    
+    #forward passs
+    logits=xenc @ W
+    counts= logits.exp()
+    probs = counts/ counts.sum(1,keepdim=True)
+    loss=-probs[torch.arange(num),ys].log().mean()
+    print(loss.item())
+
+
+    #backward pass
+    W.grad=None
+    loss.backward()
+    W.data+=-50*W.grad
+ 
+ 
+#smoothing also helps the model not to overfit
+#the same for smoothing for neural net would be adjusting the weights
+
+# if we say all the w are zeros then the output would be a uniform distribution(as all outputs will be equally probable) this is not something we want but incentivizing the weights to be negative will help the model regularize
+#so we can change the loss function to regularize
+
+#when usign softmax if the weights in w are very high the output of the neurons willbe very high so the model will b very confident in its prediction 
+#which could lead to overfitting 
+#thats why incentivising weights to go to zero willhelp regualrize the model
+
+
+g=torch.Generator().manual_seed(2147483647)
+W=torch.randn((27,27),generator=g,requires_grad=True)
+xenc=F.one_hot(xs,num_classes=27).float()
+
+#gradient descent
+
+for k in range(100):
+    
+    #forward passs
+    logits=xenc @ W
+    counts= logits.exp()
+    probs = counts/ counts.sum(1,keepdim=True)
+    loss=-probs[torch.arange(num),ys].log().mean() +0.01*(W**2).mean()
+    print(loss.item())
+
+
+    #backward pass
+    W.grad=None
+    loss.backward()
+    W.data+=-50*W.grad
+ 
+
+'''
+the loss function now has the extra term 
+0.01*( W**2).mean()
+
+we square the weights so that the weights dont becom very  negative or  very positive 
+we take the mean of all these weights as W is a matrix
+then we multiply it by the regularization parameter 0.01  
+
+
+just like when we added  a dummy value for all the bigrams by smoothing it with 1
+the smoothing helps unifom the distribution of outputs if we add a very large value the distribution will become very uniform
+similiarly if we increase the regularization parameter to say 1000000 the weights part will dominate the loss functino and weights will be near zero and the distribution eill be negative
+
+therefore the amount we add while smoothing is the same as the regularization parameter
+
+'''
+
+
+g=torch.Generator().manual_seed(2147483647)
+
+#sampling from the neural network
+for i in range (5):
+    out=[]
+    ix=0
+    while True:
+    #     #before how we calculated
+    #     p=P[ix]
+        
+        #now
+        xenc =F.one_hot(torch.tensor([ix]),num_classes=27).float()
+        logits=xenc @ W 
+        counts=logits.exp()
+        p=counts / counts.sum(1,keepdim=True)
+        
+        # #common part
+        ix=torch.multinomial(p,num_samples=1,replacement=True,generator=g).item()
+        out.append(itos[ix])
+        if ix==0:
+            break
+    print(''.join(out))
